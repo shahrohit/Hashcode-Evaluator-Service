5f8c00ebc03df8f6a9df371309556142e112ea8f
Rohit Shah <shahrohit2061@gmail.com>
2024-05-03 12:07:25 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { PORT } from "./config/server.config";
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`server is started port at ${PORT}`);
+});
