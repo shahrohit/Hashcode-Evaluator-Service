108d944da50629aed3933d6eb30e471d400ef7f7
Rohit Shah <shahrohit2061@gmail.com>
2024-05-03 13:52:17 +0530

    ping check controller

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 
 import { PORT } from "./config/server.config";
+import apiRouter from "./routes/index.route";
 const app = express();
+
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`server is started port at ${PORT}`);
